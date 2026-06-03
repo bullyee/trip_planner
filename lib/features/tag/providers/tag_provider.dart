d2e7015b651fd2d3354de1915cf1e277dedcffcc
ck10800766 <rainbow92930@gmail.com>
2026-06-03 10:25:34 +0800

    refactor(domain): decouple poi queries from tag domain and finalize architecture

--- lib/features/tag/providers/tag_provider.dart
+++ lib/features/tag/providers/tag_provider.dart
@@ -1,6 +1,5 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../poi/models/poi_model.dart';
 import '../models/tag_model.dart';
 import '../repositories/tag_repository.dart';
 
@@ -19,16 +18,4 @@ Stream<TagModel?> tagById(TagByIdRef ref, String id) {
 @riverpod
 Stream<List<TagModel>> tagsForPoi(TagsForPoiRef ref, String poiId) {
   return ref.watch(tagRepositoryProvider).watchTagsForPoi(poiId);
-}
-
-// TODO (Tech Debt): These two providers return POI data and should ideally 
-// be refactored into the POI domain (poi_provider.dart) in the future.
-@riverpod
-Stream<List<PoiModel>> poisByTag(PoisByTagRef ref, String tagId) {
-  return ref.watch(tagRepositoryProvider).watchPoisByTag(tagId);
-}
-
-@riverpod
-Stream<int> poiCountForTag(PoiCountForTagRef ref, String tagId) {
-  return ref.watch(tagRepositoryProvider).watchPoiCountForTag(tagId);
 }
